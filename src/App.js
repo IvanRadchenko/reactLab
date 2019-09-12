326d26dfc9450bc9ab15d6f8362f5b30273f17fe
Ivan Radchenko <radchenk@gmail.com>
2019-09-12 20:06:54 +0300

    Build first React component the simple way

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 
-const Pet = ({
-  name,
-  animal,
-  breed
-}) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
-  ])
-}
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
 
 const App = () => {
   return React.createElement(
-    "div", {
-      id: 'something-important'
-    }, [
+    "div",
+    {
+      id: "something-important"
+    },
+    [
       React.createElement("h1", {}, "Adopt Me!"),
       React.createElement(Pet, {
-        name: 'Luna',
-        animal: 'Dog',
-        breed: 'Havanese'
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
       }),
       React.createElement(Pet, {
-        name: 'Pepper',
-        animal: 'Bird',
-        breed: 'Cockatiel'
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
       }),
       React.createElement(Pet, {
-        name: 'Doink',
-        animal: 'Cat',
-        breed: 'Mixed'
-      }),
+        name: "Doink",
+        animal: "Cat",
+        breed: "Mixed"
+      })
     ]
   );
 };
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
 
 export default App;
