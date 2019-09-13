d9f0758b789b69dc0db4485761d42d221d7120dc
Ivan Radchenko <radchenk@gmail.com>
2019-09-13 13:53:34 +0300

    Create hooks to update App search animal, breed states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import "./App.css";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement(
+  /* Pure react syntax
+   return React.createElement(  
     "div",
     {
       id: "something-important"
@@ -28,8 +29,25 @@ const App = () => {
       })
     ]
   );
+}; */
+
+  /* JSX syntax with component reusage  
+ return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  ); */
+  return (
+    <div>
+      <h1 id="something-important">Adopt me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
 
 export default App;
