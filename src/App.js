270a7a1a543143734689b52c6d5dc035c5820f03
Ivan Radchenko <radchenk@gmail.com>
2019-09-17 22:30:57 +0300

    Add counter class statefull component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import "./App.css";
-import SearchParams from "./SearchParams";
+import Counter from "./Counter";
 
 const App = () => {
-  /* Pure react syntax
-   return React.createElement(  
-    "div",
-    {
-      id: "something-important"
-    },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed"
-      })
-    ]
-  );
-}; */
-
-  /* JSX syntax with component reusage  
- return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
-    </div>
-  ); */
   return (
     <div>
-      <h1 id="something-important">Adopt me!</h1>
-      <SearchParams />
+      <h1 id="something-important">Examples</h1>
+      <Counter title="Counter test"/>
     </div>
   );
 };
