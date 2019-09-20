335e4bc68a391f52eab90ac803bd646cc2b4c732
Ivan Radchenko <radchenk@gmail.com>
2019-09-20 12:44:14 +0300

    reactv4: Fetch and process pet info

--- src/reactv4/Pet.js
+++ src/reactv4/Pet.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 export default class Pet extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+    //Condition to pick only proper photos from a messy API array list of photos
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
     return (
-      <div>
-        <h1>{this.props.name}</h1>
-        <h2>{this.props.animal}</h2>
-        <h2>{this.props.breed}</h2>
+      <div className="pet">
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
       </div>
     );
   }
