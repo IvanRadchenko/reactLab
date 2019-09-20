5e0bbdecbba40aaa61ac6e440444a2d3ec05b2c4
Ivan Radchenko <radchenk@gmail.com>
2019-09-20 12:23:07 +0300

    Setup petfinder api v.1 the old way

--- src/reactv4/App.js
+++ src/reactv4/App.js
@@ -1,12 +1,44 @@
 import React from "react";
 import { render } from "react-dom";
 import "../App.css";
+import pf from "petfinder-client";
 import Pet from "./Pet.js";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      pets: []
+    };
   }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets: pets
+        });
+      });
+  }
+
   render() {
     return (
       <div>
