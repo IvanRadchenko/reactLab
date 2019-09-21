ec7ae35ecb6dea76b3c79f88614fdd200b80325b
Ivan Radchenko <radchenk@gmail.com>
2019-09-22 00:28:58 +0300

    Add Search Params component

--- src/reactv5/App.js
+++ src/reactv5/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import styles from "../App.css";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 export default function App() {
   return (
     <div>
       <h1 id="something-important">Adop me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havan" />
-      <Pet name="Bibo" animal="Cat" breed="Lionido" />
-      <Pet name="Doink" animal="Bird" breed="Barbero" />
+      <SearchParams />
     </div>
   );
 }
