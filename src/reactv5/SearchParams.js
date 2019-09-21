9e5b0728fce5e2feacb8788359868530dc07c258
Ivan Radchenko <radchenk@gmail.com>
2019-09-22 00:28:58 +0300

    reactv5: Add Search Params component

---
+++ src/reactv5/SearchParams.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Pet from "./Pet";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  //   const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+
+  return (
+    <div className="search-params">
+      <h1>{location}</h1>
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={e => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          ></input>
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => setAnimal(e.target.value)}
+            onBlur={e => setAnimal(e.target.value)}
+          >
+            <option>All Animals</option>
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
